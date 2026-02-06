b04d15221c0ec5fa8bcc18354d26bf9f93904b71
Plaaasma <lmedley092@gmail.com>
2026-02-06 15:25:03 -0600

    Fix Fabric permissions for singleplayer owner and op checks

--- worldedit-fabric/src/upstream/java/com/sk89q/worldedit/fabric/FabricPermissionsProvider.java
+++ worldedit-fabric/src/upstream/java/com/sk89q/worldedit/fabric/FabricPermissionsProvider.java
@@ -40,8 +40,12 @@ public interface FabricPermissionsProvider {
         @Override
         public boolean hasPermission(ServerPlayer player, String permission) {
             FabricConfiguration configuration = platform.getConfiguration();
+            boolean singlePlayerOwner = !player.server.isDedicatedServer()
+                && player.server.isSingleplayerOwner(player.getGameProfile());
+            boolean operator = player.hasPermissions(player.server.getOperatorUserPermissionLevel());
             return configuration.cheatMode
-                || player.server.getPlayerList().isOp(player.getGameProfile())
+                || singlePlayerOwner
+                || operator
                 || (configuration.creativeEnable && player.gameMode.getGameModeForPlayer() == GameType.CREATIVE);
         }
 
